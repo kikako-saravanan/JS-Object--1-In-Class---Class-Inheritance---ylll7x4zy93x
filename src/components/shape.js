5fa8ab0e5336cbdf965946847d277d0eb80f53c8
saravanan m <msaravanan1998@gmail.com>
2022-02-09 15:01:37 +0530

    update shape.js

--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,18 @@
 // declare class
+class Shape{
+  let color="black";
 
+  drawShape(){
+
+  }
+
+  calculateArea(){
+
+  }
+
+}
 // export class using module.exports
+
+module.exports = {
+    Shape: Shape
+}
