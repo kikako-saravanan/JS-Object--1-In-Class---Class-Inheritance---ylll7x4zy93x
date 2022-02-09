56b942f2f4dfd296198f8052729ddc889f09d016
saravanan m <msaravanan1998@gmail.com>
2022-02-10 01:04:23 +0530

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
-import Shape from ../shape.js
-import Circle from../circle.js
 ReactDOM.render(<App />, document.getElementById("root"));
